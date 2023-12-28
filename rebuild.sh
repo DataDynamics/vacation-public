89ec6a3f4a403d7f9680be513b574e5f12971a4a
Haneul Kim <hskimsky@gmail.com>
2023-12-28 13:23:09 +0900

    rebuild script 추가

---
+++ rebuild.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for f in $(ls .) ; do
+  if [ ${f} = "README.md" -o ${f} = "rebuild.sh" -o ${f} = "rebuild.sh" ] ; then
+    continue
+  fi
+  rm_candidate=${f}
+  rm -rf ${rm_candidate}
+done
+
+cp -R ../vacation/frontend/build/* .
+git add .
